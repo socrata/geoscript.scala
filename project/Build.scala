d2c2de127a516eb226fece0685f3e5cf336ee55e
Sam Reep <50592945+sareep@users.noreply.github.com>
2026-03-30 14:15:20 -0700

    EN-87494 Downgrade to Java 17 (#4)
    * Downgrade to Java 17
    
    * increment build version
    
    ---------
    
    Co-authored-by: Sam Reep <sam.reep@tylertech>

--- project/Build.scala
+++ project/Build.scala
@@ -9,10 +9,10 @@ object GeoScript {
   val meta =
     Seq[Setting[_]](
       organization := "com.socrata",
-      version := "0.8.5",
+      version := "0.8.6",
       scalaVersion := "2.12.21",
       scalacOptions ++= Seq("-feature", "-deprecation", "-Xlint", "-unchecked"),
-      javacOptions ++= Seq("--release", "25"),
+      javacOptions ++= Seq("--release", "17"),
       publishTo := Some(Resolver.file("file", file("release")))
     )
 
